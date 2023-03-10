9f50c796add72a0115ba550b5f86d0da586d9a76
Nikolay Trofimov <nikktro@gmail.com>
2023-03-10 21:18:02 +0300

    Initial Commit

---
+++ Online Shop/Online_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_ShopApp.swift
+//  Online Shop
+//
+//  Created by Nikolay Trofimov on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Online_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
